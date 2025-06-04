0f5da31b9f58365067073df32c425c1debb052ef
pi8878 <simangolwalifwatila99@gmail.com>
2025-06-04 15:42:44 -0500

    companion cards first done

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,47 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
+import CompanionCard from './companions/CompanionCard'
+import CompanionsList from './companions/CompanionsList'
+import CTA from './companions/CTA'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas App</h1>
-      <Button>
-        Let's get started
-      </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          name="Neura the Brainy Explorer"
+          topic="Neural Network of the Brain"
+          subject="Science"
+          duration={45}
+          color="#ffda6e"
+        />
+        <CompanionCard
+          id="456"
+          name="Countsy the Number Wizard"
+          topic="Derivative & Integrals"
+          subject="Mathematics"
+          duration={30}
+          color="#e5d0ff"
+        />
+        <CompanionCard
+          id="789"
+          name="Verba the Vocabulary Builder"
+          topic="language"
+          subject="English Literature"
+          duration={30}
+          color="#BDE7FF"
+        />
+
+      </section>
+
+      <section className="home-section">
+        <CompanionsList/>
+        <CTA/>
+      </section>
+    </main>
   )
 }
 
