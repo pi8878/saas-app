e6b32fe210ccb5e72602b79c33ed91d2682f675f
pi8878 <simangolwalifwatila99@gmail.com>
2025-06-09 22:40:07 -0500

    implement first part of auth login using clerk

--- app/companions/Navbar.tsx
+++ app/companions/Navbar.tsx
@@ -1,26 +1,36 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import NavItems from './NavItems'
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-        <Link href="/">
-            <div className="flex items-center gap-2.5 cursor-pointer">
-                <Image 
-                    src="/images/logo.svg"
-                    alt="logo"
-                    width={46}
-                    height={44}
-                />
+    return (
+        <nav className="navbar">
+            <Link href="/">
+                <div className="flex items-center gap-2.5 cursor-pointer">
+                    <Image
+                        src="/images/logo.svg"
+                        alt="logo"
+                        width={46}
+                        height={44}
+                    />
+                </div>
+            </Link>
+            <div className="flex items-center gap-8">
+                <NavItems />
+                <SignedOut>
+                    <SignInButton>
+                        <button className="btn-signin">Sign In</button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton afterSignOutUrl="/" />
+                </SignedIn>
             </div>
-        </Link>
-        <div className="flex items-center gap-8">
-            <NavItems />
-            <p>Sign In</p>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
